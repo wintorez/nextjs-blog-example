b42cbb600c5902ff7f604c951f0d074bfd1b1da6
Reza Sadr <wintorez@gmail.com>
2025-06-11 12:47:42 -0400

    Fetch and display posts from API with updated layout in Posts page

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,7 +1,25 @@
-export default function Posts() {
+export default async function Posts() {
+  const data = await fetch(
+    'https://dummyjson.com/posts?limit=10&skip=0&select=id,title,body',
+  ).then((res) => res.json())
+
   return (
-    <div className="text-center pt-12">
-      <h1 className="text-3xl capitalize font-bold mb-4">Posts</h1>
+    <div className="pt-12">
+      <h1 className="text-3xl capitalize font-bold mb-4 m-x-auto text-center">
+        Welcome to my blog!
+      </h1>
+      <div className="max-w-3xl mx-auto px-4">
+        <ul className="space-y-6">
+          {data.posts.map(
+            (post: { id: number; title: string; body: string }) => (
+              <li key={post.id} className="border p-4 rounded-lg">
+                <h2 className="text-xl font-semibold mb-2">{post.title}</h2>
+                <p className="text-gray-700">{post.body}</p>
+              </li>
+            ),
+          )}
+        </ul>
+      </div>
     </div>
   )
 }
