1283e6f4e0524e3cc3c52e41f5dcdc7afcca43a8
Reza Sadr <wintorez@gmail.com>
2025-06-11 12:20:12 -0400

    Enhance layout and content structure in Home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
         <Header />
-        <main className="flex-1">{children}</main>
+        <main className="flex-1 max-w-[900px] mx-auto">{children}</main>
         <Footer />
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,65 @@
 export default function Home() {
   return (
-    <div className="text-center pt-12">
-      <h1 className="text-3xl capitalize font-bold mb-4">
+    <div className="pt-12">
+      <h1 className="text-3xl capitalize font-bold mb-4 m-x-auto text-center">
         Welcome to my blog!
       </h1>
-      <p className="text-[16px]">This is a Next.js blog.</p>
+      <p className="text-lg mb-5">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae
+        sapien accumsan, euismod dolor sed, euismod tellus. Nulla lacinia ex
+        vitae nunc viverra, id suscipit neque viverra. Suspendisse vestibulum
+        congue sodales. Quisque vitae mauris vitae sapien pellentesque commodo.
+        Aenean eu nibh eu risus semper tincidunt. Proin eleifend sem eros, ac
+        commodo augue placerat at. Aliquam a ipsum porta, mattis neque ac,
+        consequat nibh. Aliquam erat nisi, elementum eu sapien quis, dictum
+        malesuada mi. Nullam vitae diam lorem. Etiam vitae efficitur ipsum.
+        Phasellus auctor laoreet ornare. Aenean ante erat, venenatis nec commodo
+        in, ultricies quis erat. Suspendisse faucibus placerat justo id
+        eleifend. Curabitur consectetur mi a dolor venenatis, nec ultrices
+        lectus suscipit.
+      </p>
+      <p className="text-lg mb-4">
+        Proin venenatis elementum elit non imperdiet. Sed nec tellus neque.
+        Donec nulla libero, sagittis eu pharetra in, molestie vel nisl. Quisque
+        magna arcu, vulputate quis elit ut, dapibus congue odio. Duis tincidunt
+        felis ac mattis congue. Donec in arcu justo. Morbi ut lorem dignissim,
+        accumsan nisi quis, accumsan augue. Duis dapibus pulvinar commodo. Proin
+        at porttitor mauris. Ut aliquam enim ut neque interdum molestie. Quisque
+        ut risus quis ipsum ultrices tempus et ac odio. Maecenas metus nulla,
+        iaculis at dolor eu, posuere commodo libero. Vestibulum augue eros,
+        suscipit vel quam iaculis, imperdiet vulputate nunc. Curabitur auctor
+        eget urna ac venenatis.
+      </p>
+      <p className="text-lg mb-4">
+        Etiam sagittis pharetra arcu ut pretium. Proin pretium, leo ac gravida
+        bibendum, justo eros porta eros, consectetur malesuada risus metus ac
+        lectus. Nunc ultrices nulla eu mauris porta auctor. Vivamus in tortor
+        metus. Donec finibus quam in neque dignissim vehicula. Morbi fermentum
+        est massa, sed rutrum nisl iaculis sed. Interdum et malesuada fames ac
+        ante ipsum primis in faucibus. Integer ac leo in lacus consequat
+        bibendum. Proin dapibus tellus id dolor viverra hendrerit. Nulla maximus
+        justo nec ligula consequat maximus.
+      </p>
+      <p className="text-lg mb-4">
+        Nulla ultrices nisl nec euismod imperdiet. In interdum risus quis erat
+        convallis dapibus. Fusce sit amet finibus nunc. Vestibulum interdum
+        varius faucibus. Fusce sagittis tellus vitae elit dictum, eget lacinia
+        ligula posuere. Donec non luctus justo, et porta tortor. Cras sed tempus
+        mauris, ut sagittis sapien. Maecenas ac suscipit quam. Maecenas volutpat
+        laoreet elit quis rhoncus. Pellentesque a accumsan eros.
+      </p>
+      <p className="text-lg mb-4">
+        Fusce bibendum turpis et augue mollis eleifend. Quisque volutpat ligula
+        tellus, at rutrum felis interdum non. Nam vitae tincidunt lorem, eu
+        vestibulum libero. Nulla elementum vitae metus ut malesuada. Cras vel
+        ornare felis, porttitor tempus leo. Ut lobortis ac augue sed rhoncus.
+        Mauris nec purus lobortis, euismod lectus vitae, elementum orci. Nulla
+        facilisi. Etiam ut sapien volutpat, blandit enim iaculis, lacinia
+        mauris. Donec pellentesque ultricies est nec maximus. Nunc ac aliquet
+        libero, eu consectetur lorem. Integer et eleifend lacus. Donec venenatis
+        egestas massa tincidunt finibus. Vivamus sit amet metus arcu. Curabitur
+        iaculis condimentum sem non eleifend.
+      </p>
     </div>
   )
 }
